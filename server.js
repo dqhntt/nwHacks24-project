3ef6c63ab8cdc9595a2612fa8eab78a21039445b
Ayush Patel <104913698+Ayush200423@users.noreply.github.com>
2024-01-20 15:11:55 -0800

    forgot listening port

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "success" })
+    res.status(200).json({ message: "success" });
 })
+
+app.listen(3000);
