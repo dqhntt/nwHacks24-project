f9d345f07a897f90dee46a154d2314685ba47139
mantajdhillon <mantajdhillon91@gmail.com>
2024-01-20 21:06:23 -0800

    Abstracted the NavBar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,44 +1,13 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
-import * as Icon from 'react-bootstrap-icons';
+import Navigation from "../components/Navigation";
 
 
 
 export default function Home() {
   return (
     <div className="home">
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">TaskTrover</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="./Home">Home</Nav.Link>
-              <Nav.Link href="/Leaderboards">Leaderboard</Nav.Link>
-              <Nav.Link href="/Rewards">Rewards</Nav.Link>
-              <Nav.Link href="/Settings">Settings</Nav.Link>
-              <Nav.Link href="https://github.com/dqhntt/nwHacks24-project">GitHub</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          <Navbar.Collapse className="justify-content-end">
-            <Row>
-              <Col xs={6} md={4}>
-              <Icon.Plus color="black" size={35} />
-              </Col>
-              <Col xs={6} md={4}>
-                <Icon.Person color="black" size={30} />
-              </Col>
-            </Row>
-        </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navigation></Navigation>
     </div>
   );
 }
