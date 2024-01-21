012f92cbfd66ed528b1f91530a2a1099d7f56bc7
mantajdhillon <mantajdhillon91@gmail.com>
2024-01-20 20:49:50 -0800

    Developed NavBar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,11 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import * as Icon from 'react-bootstrap-icons';
+
 
 
 export default function Home() {
@@ -11,27 +16,27 @@ export default function Home() {
     <div className="home">
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">TaskTrover</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="./Home">Home</Nav.Link>
+              <Nav.Link href="/Leaderboards">Leaderboard</Nav.Link>
+              <Nav.Link href="/Rewards">Rewards</Nav.Link>
+              <Nav.Link href="/Settings">Settings</Nav.Link>
+              <Nav.Link href="https://github.com/dqhntt/nwHacks24-project">GitHub</Nav.Link>
             </Nav>
           </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <Row>
+              <Col xs={6} md={4}>
+              <Icon.Plus color="black" size={35} />
+              </Col>
+              <Col xs={6} md={4}>
+                <Icon.Person color="black" size={30} />
+              </Col>
+            </Row>
+        </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
