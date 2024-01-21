3c6313505b46a5249ec45eb7d7e3254682c8e303
Mekdim Dereje <mda89@sfu.ca>
2024-01-20 16:24:09 -0800

    Add React Router and create initial pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Leaderboards from "./pages/Leaderboards";
+import Rewards from "./pages/Rewards";
+import Settings from "./pages/Settings";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>gjgjgjgjg</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="leaderboards" element={<Leaderboards />} />
+            <Route path="rewards" element={<Rewards />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ frontend/src/pages/Leaderboards.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Leaderboards() {
+  return (
+    <div>
+      <h1>Leaderboards</h1>
+    </div>
+  );
+}
